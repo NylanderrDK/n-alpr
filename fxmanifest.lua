d64c9b3306118e7c3f7d2cb67bac8c35fca0412c
Hjalte Nylander Larsen <hjalteantondk@gmail.com>
2021-12-08 18:39:55 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game "gta5"
 
 author "Nylander"
 description "ALPR extension to QBCore, with exports from Wraith ARS 2X."
-version "1.0.0"
+version "1.1.0"
 
 client_scripts { "./cl_*.lua" }
 server_scripts { "./sv_*.lua" }
